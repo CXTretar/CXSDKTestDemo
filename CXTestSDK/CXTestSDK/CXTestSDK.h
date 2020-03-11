8dbd5f41fbfca54843e143df2b859d2d3f901dc8
CXTretar <misscxuan@163.com>
2020-03-11 22:59:08 +0800

    创建sdk项目，并且通过pod导入SVProgressHUD 框架。

---
+++ CXTestSDK/CXTestSDK/CXTestSDK.h
@@ -0,0 +1,19 @@
+//
+//  CXTestSDK.h
+//  CXTestSDK
+//
+//  Created by CXTretar on 2020/3/11.
+//  Copyright © 2020 CXTretar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CXTestSDK.
+FOUNDATION_EXPORT double CXTestSDKVersionNumber;
+
+//! Project version string for CXTestSDK.
+FOUNDATION_EXPORT const unsigned char CXTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXTestSDK/PublicHeader.h>
+
+
